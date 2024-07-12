90dca44667e94116bb987a53d57902614405bf61
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-07-12 06:55:34 +0530

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Dilpreet Singh on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
